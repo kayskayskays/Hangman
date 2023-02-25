4cfa54cb0574d7d378b0fdd4fbe496cb008d9cb1
sskki <beaver.hacker@gmail.com>
2023-02-25 20:42:24 +1100

    added newline

--- likeAHorse.py
+++ likeAHorse.py
@@ -3,27 +3,27 @@
 def drawHangman(i):
     match i:
         case 0:
-            return '          \n          \n          \n          \n           \n          \n=========='
+            return '\n          \n          \n          \n          \n           \n          \n=========='
         case 1:
-            return '          \n  |       \n  |       \n  |       \n  |        \n  |       \n=========='
+            return '\n          \n  |       \n  |       \n  |       \n  |        \n  |       \n=========='
         case 2:
-            return ' +---+   \n  |       \n  |       \n  |       \n  |        \n  |       \n=========='
+            return '\n +---+   \n  |       \n  |       \n  |       \n  |        \n  |       \n=========='
         case 3:
-            return ' +---+   \n  |/      \n  |       \n  |       \n  |        \n  |       \n=========='
+            return '\n +---+   \n  |/      \n  |       \n  |       \n  |        \n  |       \n=========='
         case 4:
-            return ' +---+   \n  |/    |   \n  |       \n  |       \n  |        \n  |       \n=========='
+            return '\n +---+   \n  |/    |   \n  |       \n  |       \n  |        \n  |       \n=========='
         case 5:
-            return ' +---+   \n  |/    |   \n  |     O   \n  |       \n  |        \n  |       \n=========='
+            return '\n +---+   \n  |/    |   \n  |     O   \n  |       \n  |        \n  |       \n=========='
         case 6:
-            return ' +---+   \n  |/    |   \n  |     O   \n  |     |   \n  |        \n  |       \n=========='
+            return '\n +---+   \n  |/    |   \n  |     O   \n  |     |   \n  |        \n  |       \n=========='
         case 7:
-            return ' +---+   \n  |/    |   \n  |     O   \n  |     |   \n  |   /    \n  |       \n=========='
+            return '\n +---+   \n  |/    |   \n  |     O   \n  |     |   \n  |   /    \n  |       \n=========='
         case 8:
-            return ' +---+   \n  |/    |   \n  |     O   \n  |     |   \n  |   / \\  \n  |       \n=========='
+            return '\n +---+   \n  |/    |   \n  |     O   \n  |     |   \n  |   / \\  \n  |       \n=========='
         case 9:
-            return ' +---+   \n  |/    |   \n  |     O   \n  |    /|   \n  |   / \\  \n  |       \n=========='
+            return '\n +---+   \n  |/    |   \n  |     O   \n  |    /|   \n  |   / \\  \n  |       \n=========='
         case 10:
-            return ' +---+   \n  |/    |   \n  |     O   \n  |    /|\\  \n  |   / \\  \n  |       \n=========='
+            return '\n +---+   \n  |/    |   \n  |     O   \n  |    /|\\  \n  |   / \\  \n  |       \n=========='
         case _:
             return 'somebody fricked up while programming\n\n\n\n\n\n'
 
@@ -38,7 +38,7 @@ def generateDisplay(word, guesses):
         else:
             wrong.append(g)
     if len(wrong)>10:
-        return ' +---+\n  |/    |\n  |     O\n  |    /|\\\n  |   /¶\\\n  |\n=========='
+        return '\n +---+\n  |/    |\n  |     O\n  |    /|\\\n  |   /¶\\\n  |\n=========='
     # Generate the guess line
     gLine = ''
     for w in word:
